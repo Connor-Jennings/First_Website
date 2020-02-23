bcee8a35f62b98c4cd7d68dd0b74657a152560b7
Connorshub <jennings.co.d@gmail.com>
2020-02-23 11:10:51 -0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,24 +6,25 @@
 var counter = 0;
 $('h2.gallery').on('click', function(){
    if((counter%2) == 0){
-        $('div.photos').children('p').hide();
-        counter +=1;
-        if(counter === 1){
-         $.getJSON('data/photos.json').done(function(data){
-           var newContent = '';
-           for(var i=0; i<data.photoList.length; i++){
-              newcontent += '<img src="'+ data.events[i].src +'" ';
-              newcontent += 'alt ="' + data.events[i].alt +'" ';
-              newContent += 'width ="' + data.events[i].width +'" ';
-              newContent += 'height ="' + data.events[i].height +'" />';
-           }
-           $('div#imgs').html(newContent);
-         }).fail(function(){
+      $('div.photos').children('p').hide();
+      counter +=1;
+      if(counter === 1){
+         var newContent = '';
+         $.getJSON('data/photos.json')
+         .done(function(data){
+            for(var i=0; i<data.photoList.length; i++){
+               newcontent += '<img src="'+ data.events[i].src +'" ';
+               newcontent += 'alt ="' + data.events[i].alt +'" ';
+               newContent += 'width ="' + data.events[i].width +'" ';
+               newContent += 'height ="' + data.events[i].height +'" />';
+            }
+            $('div#imgs').html(newContent);})
+         .fail(function(){
             $('div#imgs').html('<p>Load failed<p');
             });}
-         else{
-            $('div#imgs').show();
-         }
+      else{
+         $('div#imgs').show();
+      }
    }
    else{
         $('div#imgs').hide();

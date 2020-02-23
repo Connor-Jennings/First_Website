253fc8ca3c591e257eba72f613fb5bd5995646c5
Connorshub <jennings.co.d@gmail.com>
2020-02-23 11:40:58 -0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,20 @@
 
 
 
+$(function(){
+   var counter = 0;
+   $.ajax({
+      beforeSend: function(xhr){
+         if(xhr.ovverideMimeType){
+            xhr.ovverideMimeType("application/json");
+         }
+      }
+   });
 
-//function to toggle between loading in photos and obama 
-var counter = 0;
-$('h2.gallery').on('click', function(){
-   if((counter%2) == 0){
-      $('div.photos').children('p').hide();
-      counter +=1;
-      if(counter === 1){
-         var newContent = '';
-         $.getJSON('data/photos.json')
+   function loadImages(){
+      $.getJSON('data/photos.json')
          .done(function(data){
+            var newContent = '';
             for(var i=0; i<data.photoList.length; i++){
                newContent += '<img src="'+ data.photoList[i].src +'" ';
                newContent += 'alt ="' + data.photoList[i].alt +'" ';
@@ -20,15 +23,26 @@ $('h2.gallery').on('click', function(){
             }
             $('div#imgs').html(newContent);})
          .fail(function(){
-            $('div#imgs').html('<p>Load failed<p');
-            });}
-      else{
-         $('div#imgs').show();
+            $('div#imgs').html('<p>Load failed<p>');
+            });
+   }
+
+   $('h2.gallery').on('click', function(){
+      if((counter%2) == 0){
+         $('div.photos').children('p').hide();
+         counter +=1;
+         if(counter === 1){
+            loadImages();
+         }
+         else{
+            $('div#imgs').show();
+         }
       }
-   }
-   else{
-        $('div#imgs').hide();
-        $('div.photos').children('p').show();
-        counter+=1;
-   }
+      else{
+           $('div#imgs').hide();
+           $('div.photos').children('p').show();
+           counter+=1;
+      }
+   });
+
 });

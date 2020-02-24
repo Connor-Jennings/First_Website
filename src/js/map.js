a67e45d73db2440baf72bba4b4ae9f418c72ad58
Connorshub <jennings.co.d@gmail.com>
2020-02-23 20:58:28 -0800

    Update map.js

--- src/js/map.js
+++ src/js/map.js
@@ -14,22 +14,19 @@ $(function(){                                     //when DOM is ready
   function LoadMap(){                                //map initilization
     $.getJSON('data/testTrip.json')
     .done(function(data){
-			var latitude = parseFloat(data.trip[0].lat);
-			var longitude = parseFloat(data.trip[0].lng);
-      var uluru = {lat: latitude, lng: longitude};  
+			var latLng = new google.maps.LatLng(parseFloat(data.trip[0].lat),parseFloat(data.trip[0].lng)); 
       var map = new google.maps.Map(                  // The map, centered at Uluru
-        document.getElementById('map'), {zoom: 4, center: uluru });
+        document.getElementById('map'), {zoom: 10, center: LatLng });
       
-				var latLng = new google.maps.LatLng(parseFloat(data.trip[0].lat),parseFloat(data.trip[0].lng));
-				var marker = new google.maps.Marker({
-						position: latLng,
-						map: map
-        });
-				var latLn = new google.maps.LatLng(2.8,-187.3);
-				var marker = new google.maps.Marker({
-						position: latLn,
-						map: map
-        });
+			var marker = new google.maps.Marker({
+					position: latLng,
+					map: map
+			});
+			var latLn = new google.maps.LatLng(2.8,-187.3);
+			var marker = new google.maps.Marker({
+					position: latLn,
+					map: map
+			});
     })
     .fail(function(){
       $('div.messageBoard').html('it failed to load');

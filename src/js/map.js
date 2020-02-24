eb46de26e576a1cee2794539325974e7cdc212aa
Connorshub <jennings.co.d@gmail.com>
2020-02-23 21:00:04 -0800

    Update map.js

--- src/js/map.js
+++ src/js/map.js
@@ -16,7 +16,7 @@ $(function(){                                     //when DOM is ready
     .done(function(data){
 			var latLng = new google.maps.LatLng(parseFloat(data.trip[0].lat),parseFloat(data.trip[0].lng)); 
       var map = new google.maps.Map(                  // The map, centered at Uluru
-        document.getElementById('map'), {zoom: 100, center: LatLng });
+        document.getElementById('map'), {zoom: 100, center: latLng });
       
 			var marker = new google.maps.Marker({
 					position: latLng,

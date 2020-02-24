c314ce38ea8f63d99defea9577d4a732e43f0222
Connorshub <jennings.co.d@gmail.com>
2020-02-23 19:42:36 -0800

    map updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,42 @@
 
+//This page contains the JavaScript for the Home page 
 
 
-$(function(){
-   var counter = 0;
+$(function(){                                         //When the DOM is ready
+   var counter = 0;                 //for toggling between photos and obama
+   var loadFailed = false;          //for loading photos
    $.ajax({
       beforeSend: function(xhr){
-         if(xhr.ovverideMimeType){
-            xhr.ovverideMimeType("application/json");
+         if(xhr.ovverideMimeType){                    //If supported
+            xhr.ovverideMimeType("application/json"); // Set MIME to prevent errors
          }
       }
    });
 
-   function loadImages(){
-      $.getJSON('data/photos.json')
-         .done(function(data){
+   function LoadImages(){
+      $.getJSON('data/photos.json')                   //Try to collect JSON data
+         .done(function(data){                        //Build image elements
             var newContent = '';
-            for(var i=0; i<data.photoList.length; i++){
+            for(var i=0; i<data.photoList.length; i++){              
                newContent += '<img class="picture" src="'+ data.photoList[i].src +'" ';
                newContent += 'alt ="' + data.photoList[i].alt +'" ';
                newContent += 'width ="' + data.photoList[i].width +'" ';
                newContent += 'height ="' + data.photoList[i].height +'" />';
             }
-            $('div#imgs').html(newContent);})
+            $('div#imgs').html(newContent);
+            loadFailed = false;})                       //Insert images
          .fail(function(){
             $('div#imgs').html('<p>Load failed<p>');
+            loadFailed = true;                          //asserts load will try again if user tries again
             });
    }
 
-   $('h2.gallery').on('click', function(){
+   $('h2.gallery').on('click', function(){         //event listener for photos
       if((counter%2) == 0){
          $('div.photos').children('p').hide();
          counter +=1;
-         if(counter === 1){
-            loadImages();
+         if(counter === 1 || loadFailed){          //only load in images once 
+            LoadImages();
          }
          else{
             $('div#imgs').show();
--- src/js/map.js
+++ src/js/map.js
@@ -1,29 +1,42 @@
 
 //This page contains the JavaScript for the map page 
 
-//map initilization 
-$(function() {
-   var uluru = {lat: 37.73171, lng: -119.558694};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-
-});
-
-
-
-
-
-//removes links in menu after hovering
-$(function() {
- 
-   var $listItems = $('li');
+$(function(){                                     //when DOM is ready
   
-   $listItems.on('mouseout', function(){
-      $(this).children('a').remove();
+  $.ajax({
+      beforeSend: function(xhr){
+        if(xhr.ovverideMimeType){                    //If supported
+            xhr.ovverideMimeType("application/json"); // Set MIME to prevent errors
+         }
+      }
    });
+  
+  
+  function LoadMap(selection){                                //map initilization
+    $getJSON('data/testTrip.JSON')
+    .done(function(data){
+      var uluru = {lat: data.selection[0].lat, lng: data.selection[0].lng};  
+      var map = new google.maps.Map(                  // The map, centered at Uluru
+        document.getElementById('map'), {zoom: 4, center: uluru}
+      );
+      
+      var marker = new google.maps.Marker({position: uluru, map: map}); // A marker, positioned at Uluru
+    })
+    .fail(function(){
+      $('div.messageBoard').html('it failed to load');
+    });
+  }
+  
+  LoadMap('trip');
+  
+  
+  
+  //removes items in the menu after hovering
+  $(function() {
+     var $listItems = $('li');
+     $listItems.on('mouseout', function(){
+        $(this).children('a').remove();
+     });
+  });
+
 });
-
-

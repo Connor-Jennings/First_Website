140792d100a922e415546fbefc5af8d84ea8b6f2
Connorshub <jennings.co.d@gmail.com>
2020-02-27 08:28:48 -0800

    Update map.js

--- src/js/map.js
+++ src/js/map.js
@@ -55,7 +55,7 @@ $(function(){                                     //when DOM is ready
 	LoadMenu();                        //Initialize the Menu
   LoadMap('data/testTrip.json');    //tripList.path  object in the file "tripList.json" 
   
-  $('input.name').on('click', function(){
+  $('input').on('click', function(){
     $('h3.infoTitle').html(this.name);
     });
   
